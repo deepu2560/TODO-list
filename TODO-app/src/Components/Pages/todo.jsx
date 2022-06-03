2cdf86f37cf5b9c39e193636680cccad6d6d9651
unknown <deepanshugulia256@gmail.com>
2022-06-03 19:11:48 +0530

    todo list main UI done

--- TODO-app/src/Components/Pages/todo.jsx
+++ TODO-app/src/Components/Pages/todo.jsx
@@ -5,14 +5,23 @@ export const Todomain = () => {
   return (
     <div>
       <h1>TODO list</h1>
-      <div>
-        <input type="text" />
-        <button>ADD TODO</button>
+      <div id="input-search-main">
+        <input type="text" id="input-search" />
+        <button id="button-search">ADD TODO</button>
       </div>
       <div id="todo-main-div">
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>
+          <h5>Pending</h5>
+          <div className="list-main-div"></div>
+        </div>
+        <div>
+          <h5>Doing</h5>
+          <div className="list-main-div"></div>
+        </div>
+        <div>
+          <h5>Done</h5>
+          <div className="list-main-div"></div>
+        </div>
       </div>
     </div>
   );
