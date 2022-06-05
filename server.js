ef8a72a99308828924bdb18f9202f19bf01ccf0c
unknown <deepanshugulia256@gmail.com>
2022-06-05 17:42:16 +0530

    server started

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require("express");
 
+const connect = require("./src/config/db");
+
 const app = express();
 
 app.use(express.json());
+
+app.listen(process.env.PORT || 8000, async () => {
+  try {
+    await connect();
+
+    console.log("Server started");
+  } catch (error) {
+    console.log("ERROR", error);
+  }
+});
--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connect = () => {
-  return mongoose.connect("");
+  return mongoose.connect(`${process.env.URL}`);
 };
 
 module.exports = connect;
