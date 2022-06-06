4321ffad367dcaca99967e6492e9b077252ce6f9
unknown <deepanshugulia256@gmail.com>
2022-06-06 21:40:14 +0530

    backend done

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require("express");
 
 const connect = require("./src/config/db");
 
+const cors = require("cors");
+
+const TodoController = require("./src/controllers/todoControl");
+
+const UserController = require("./src/controllers/userControl");
+
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
+
+app.use("/user", UserController);
+
+app.use("/todo", TodoController);
 
 app.listen(process.env.PORT || 8000, async () => {
   try {
--- src/controllers/todoControl.js
+++ src/controllers/todoControl.js
@@ -2,17 +2,84 @@ const express = require("express");
 
 const Event = require("../models/todoModels");
 
+const Authenticate = require("../middlewares/authenticate");
+
 const router = express.Router();
 
-router.post("/event", async (req, res) => {
+router.post("/event", Authenticate, async (req, res) => {
   try {
     let event = await Event.create(req.body);
 
     console.log(`=>> event is added for ${req.body.user_id}`);
 
-    res.status().send({ error: false, event });
+    res.status(200).send({ error: false, event });
   } catch (error) {
-    console.log("=>> Registration ERROR", error);
+    console.log("=>> Progress ERROR", error);
+    res.status(502).send({ error: true, event: "" });
+  }
+});
+
+router.get("/pending/:id", Authenticate, async (req, res) => {
+  try {
+    let event = await Event.find({
+      user_id: req.params.id,
+      progess: "pending",
+    })
+      .lean()
+      .exec();
+
+    console.log(`=>> Showing pending progress for ${req.params.id}`);
+    res.status(200).send({ error: false, event });
+  } catch (error) {
+    console.log("=>> Progress ERROR", error);
+    res.status(502).send({ error: true, event: "" });
+  }
+});
+
+router.get("/doing/:id", Authenticate, async (req, res) => {
+  try {
+    let event = await Event.find({
+      user_id: req.params.id,
+      progess: "doing",
+    })
+      .lean()
+      .exec();
+
+    console.log(`=>> Showing doing progress for ${req.params.id}`);
+    res.status(200).send({ error: false, event });
+  } catch (error) {
+    console.log("=>> Progress ERROR", error);
+    res.status(502).send({ error: true, event: "" });
+  }
+});
+
+router.get("/done/:id", Authenticate, async (req, res) => {
+  try {
+    let event = await Event.find({
+      user_id: req.params.id,
+      progess: "done",
+    })
+      .lean()
+      .exec();
+
+    console.log(`=>> Showing done progress for ${req.params.id}`);
+    res.status(200).send({ error: false, event });
+  } catch (error) {
+    console.log("=>> Progress ERROR", error);
+    res.status(502).send({ error: true, event: "" });
+  }
+});
+
+router.put("/:id", Authenticate, async (req, res) => {
+  try {
+    let event = await Event.findOneAndUpdate({ user_id: req.params.id })
+      .lean()
+      .exec();
+
+    console.log(`=>> Progress updated for ${req.params.id}`);
+    res.status(200).send({ error: false, event });
+  } catch (error) {
+    console.log("==> Progress ERROR", error);
     res.status(502).send({ error: true, event: "" });
   }
 });
